cbc794247032118a3540b7b4a1a7139a7f02af34
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-11-19 18:53:19 -0300

    deleting comments

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,6 @@
 import puppeteer from "puppeteer"
 import fs from 'fs'
-/**
- * @author  Radamantium <eduardo.arede@gmail.com>
- * 
- * 
- * @param {Object} params - The parameters for the function.
- * @param {string} params.url - The URL of the webpage to scrape.
- * @param {string} params.selector - The CSS selector to target elements.
- * @param {string} params.attr - The attribute of the targeted elements to scrape.
- * @param {string} params.jsonFileName - The name of the JSON file where results will be saved.
- */
+
 class ScrapToJson {
     url: string;
     selector: string;
