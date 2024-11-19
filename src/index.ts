e50a6564600d70b02b57360f26757c12cfff211a
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-11-18 23:58:18 -0300

    feat: class settings

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,28 @@
 import puppeteer from "puppeteer"
 import fs from 'fs'
-
-export async function getParams() {
+/**
+ * @author  Radamantium <eduardo.arede@gmail.com>
+ * 
+ * 
+ * @param {Object} params - The parameters for the function.
+ * @param {string} params.url - The URL of the webpage to scrape.
+ * @param {string} params.selector - The CSS selector to target elements.
+ * @param {string} params.attr - The attribute of the targeted elements to scrape.
+ * @param {string} params.jsonFileName - The name of the JSON file where results will be saved.
+ */
+class ScrapToJson {
+     async getScrapAllElementsAttr({url, selector, attr, jsonFileName}:{url:string, selector:string, attr: string, jsonFileName:string}) {
+    try{
     const browser =  await puppeteer.launch({
         headless:false,
       })
       const page = await browser.newPage()
-      await page.goto('https://www.twpornstars.com/?page=1') 
+      await page.goto(url) 
       const results = await page.evaluate(()=>{
         
-        const content = document.querySelectorAll('.thumb__link')
+        const content = document.querySelectorAll(selector)
         const data = [...content].map(con=>{
-            const source = { url: con.getAttribute('href')}
+            const source = { url: con.getAttribute(attr)}
             return source
            
         })
@@ -20,13 +31,52 @@ export async function getParams() {
         return data
     })
 
-    fs.writeFile('some-json.json',JSON.stringify(results),(err)=>{
+    fs.writeFile(jsonFileName,JSON.stringify(results),(err)=>{
         if(err){
             console.log(err)
         }else{
-            console.log('Data scraped')
+            console.log('JSON created')
         }
     })
     await browser.close()
+}catch(err){
+    console.log('Something went wrong!',err)
 }
+}
+
+async getScrapAllElementsInnerHTML({url, selector, jsonFileName}:{url:string, selector:string, jsonFileName:string}) {
+    try{
+    const browser =  await puppeteer.launch({
+        headless:false,
+      })
+      const page = await browser.newPage()
+      await page.goto(url) 
+      const results = await page.evaluate(()=>{
+        
+        const content = document.querySelectorAll(selector)
+        const data = [...content].map(con=>{
+            const source = { url: con.innerHTML }
+            return source
+           
+        })
+        
+        
+        return data
+    })
+
+    fs.writeFile(jsonFileName,JSON.stringify(results),(err)=>{
+        if(err){
+            console.log(err)
+        }else{
+            console.log('JSON created')
+        }
+    })
+    await browser.close()
+}catch(err){
+    console.log('Something went wrong!',err)
+}
+}
+}
+
+export default new ScrapToJson
 
