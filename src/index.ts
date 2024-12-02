ffe6a0316b46e38b255259dcedbf798ef4dc5545
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-12-01 22:41:01 -0300

    setting new structure

--- src/index.ts
+++ src/index.ts
@@ -1,132 +1,21 @@
-import puppeteer from "puppeteer"
-import fs from 'fs'
+
 
 class ScrapToJson {
-    url: string;
-    selector: string;
-    jsonFileName: string;
-    constructor(url:string, selector:string, jsonFileName:string){
-        this.url = url;
-        this.selector = selector;
-        this.jsonFileName = jsonFileName;
+  jsonFileName: string;
+  constructor(jsonFileName: string) {
+    this.jsonFileName = jsonFileName;
+  }
+  async getScrapAllElements() {
+    try {
+    } catch (err) {
+      console.log("Something went wrong!", err);
     }
-     async getScrapAllElementsAttr() {
-    try{
-    const browser =  await puppeteer.launch({
-        headless:false,
-      })
-      const page = await browser.newPage()
-      await page.goto(this.url) 
-      const results = await page.evaluate(()=>{
-        const propmpter:any = prompt("Inserte the HTML element or class","")
-        const content = document.querySelectorAll(propmpter)
-        const data = [...content].map(con=>{
-            const source = { content: con.getAttribute('href') }
-            return source
-           
-        })
-        
-        
-        return data
-    })
+  }
 
-    fs.writeFile(`${this.jsonFileName}.json`,JSON.stringify(results),(err)=>{
-        if(err){
-            console.log(err)
-        }else{
-            console.log('JSON created')
-        }
-    })
-    await browser.close()
-}catch(err){
-    console.log('Something went wrong!',err)
-}
-}
-
-async getScrapAllElementsInnerHTML() {
-    try{
-    const browser =  await puppeteer.launch({
-        headless:false,
-      })
-      const page = await browser.newPage()
-      await page.goto(this.url) 
-      const results = await page.evaluate(()=>{
-        
-        const content = document.querySelectorAll(this.selector)
-        const data = [...content].map(con=>{
-            const source = { content: con.innerHTML }
-            return source
-           
-        })
-        
-        
-        return data
-    })
-
-    fs.writeFile(`${this.jsonFileName}.json`,JSON.stringify(results),(err)=>{
-        if(err){
-            console.log(err)
-        }else{
-            console.log('JSON created')
-        }
-    })
-    await browser.close()
-}catch(err){
-    console.log('Something went wrong!',err)
-}
-}
-
-async getScrapElementAttr(attr:string){
-    try{
-        const browser =  await puppeteer.launch({
-            headless:false,
-          })
-          const page = await browser.newPage()
-          await page.goto(this.url) 
-          const results = await page.evaluate(()=>{
-            const content = document.querySelector(this.selector)
-            const data = { content:content?.getAttribute(attr) }
-            return data
-          })
-          fs.writeFile(`${this.jsonFileName}.json`,JSON.stringify(results),(err)=>{
-            if(err){
-                console.log(err)
-            }else{
-                console.log('JSON created')
-            }
-        })
-        await browser.close()
-
-    }catch(error){
-        console.log('Something went wrong!',error)
+  async getScrapSingleElement() {
+    try {
+    } catch (err) {
+      console.log("Something went wrong!", err);
     }
+  }
 }
-async getScrapElementInnerHTML(){
-    try{
-        const browser =  await puppeteer.launch({
-            headless:false,
-          })
-          const page = await browser.newPage()
-          await page.goto(this.url) 
-          const results = await page.evaluate(()=>{
-            const content = document.querySelector(this.selector)
-            const data = { content: content?.innerHTML }
-            return data
-          })
-          fs.writeFile(`${this.jsonFileName}.json`,JSON.stringify(results),(err)=>{
-            if(err){
-                console.log(err)
-            }else{
-                console.log('JSON created')
-            }
-        })
-        await browser.close()
-
-    }catch(error){
-        console.log('Something went wrong!',error)
-    }
-}
-}
-
-export default ScrapToJson
-
