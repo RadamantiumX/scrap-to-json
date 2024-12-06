1e4660f84f4495939eaa0971e90d3a08f81a5964
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-12-05 21:43:01 -0300

    feat: adding validation

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -8,8 +8,8 @@ type input = {
 
 const userPromptSchema = z.object({
     url: z.string().url({message: 'invalid url'}).startsWith('https://', {message: "Must provide secure url -E.g. https://secureweb.com"}),
-    element: z.string(),
-    file: z.string().refine((s)=> !s.includes(" "), 'Wrong file name')
+    element: z.string().min(1, {message: 'empty field'}),
+    file: z.string().min(1, {message: 'file name must have 1 character or more'}).refine((s)=> !s.includes(" "), 'Wrong file name')
 })
 
 export function validateUserPromptSchema(input:input) {
