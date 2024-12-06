52ca8014566c9fd5c76cf9491a616e9b10863fde
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2024-12-05 22:14:26 -0300

    feat: testing one change

--- src/test/loadElements.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from "vitest";
-import { loadElements } from "../lib/loadElements";
-
-describe('loadElements', ()=>{
-    it('should be return object', ()=>{
-        expect(loadElements('https://www.w3schools.com/', 'a', 'fileName','all')).toBeTypeOf('object')
-    })
-})
